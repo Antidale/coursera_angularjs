cf80f7905f3a88e661855aae54c676e9b62796f1
Scott Wyngarden <swyngarden@gmail.com>
2018-05-22 13:10:58 -0700

    using  service, breaking out the controller function

--- dependency_injection_demo/app.js
+++ dependency_injection_demo/app.js
@@ -1,7 +1,11 @@
 (() => {
   'use strict';
   angular.module('DependencyInjectionDemo', [])
-    .controller('DepInjController', () => {
+    .controller('DIController', DIController);
 
-    });
+  function DIController($scope, $filter) {
+    $scope.name = "Orange";
+
+    $scope.upper = () => $scope.name = $filter('uppercase')($scope.name);
+  }
 })();
