ab3d29a0a2a04d7c653a53d9a05302e4f1ade9b1
Scott Wyngarden <swyngarden@gmail.com>
2018-05-22 12:20:49 -0700

    adding ng-keyup

--- name_calculator/app.js
+++ name_calculator/app.js
@@ -3,8 +3,11 @@
   angular.module('NameCalculator', [])
     .controller('NameCalculatorController', function ($scope) {
       $scope.name = ""
+      $scope.totalValue = 0;
       $scope.calculateName = () => $scope.name.split('')
-        .reduce((acc, val) => acc + val.charCodeAt(0), 0)
+        .reduce((acc, val) => acc + val.charCodeAt(0), 0);
+      $scope.calculateFromKeyUp = () => $scope.totalValue = $scope.calculateName();
+
     })
 
 })();
