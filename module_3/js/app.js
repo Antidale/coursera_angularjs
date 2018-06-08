9389b39a9888fbb7c5675666343fa2ae7d1be459
Scott Wyngarden <swyngarden@gmail.com>
2018-06-08 16:39:49 -0700

    first pass for finishing module 3

--- module_3/js/app.js
+++ module_3/js/app.js
@@ -2,14 +2,69 @@
   angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItems)
 
   NarrowItDownController.$inject = ['MenuSearchService']
   function NarrowItDownController(MenuSearchService) {
+    let vm = this;
+    let message = "Nothing found"
+    vm.searchTerm = "";
+    vm.found = [];
+    vm.message = undefined;
+    vm.findItems = () => {
+
+      //Set "Nothing Found" and don't bother with executing the AJAX call
+      if (vm.searchTerm == "") {
+        vm.message = message;
+        return;
+      }
+
+      MenuSearchService.getMatchedMenuItems(vm.searchTerm)
+        .then(response => {
+          vm.found = response;
+          vm.message = response.length > 0 ? undefined : message
+        })
+        .catch(() => {
+          vm.found = []
+          vm.message = message
+        });
+
+    }
+    vm.remove = (index) => {
+      console.log("got here!")
+      vm.found.splice(index, 1)
+    }
 
   }
 
   MenuSearchService.$inject = ['$http']
   function MenuSearchService($http) {
-
+    let service = this;
+    service.getMatchedMenuItems = (searchTerm) => {
+      return $http({
+        method: 'GET',
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+      }).then(result => {
+        let foundItems = [];
+        result.data.menu_items.forEach(menuItem => {
+          if (menuItem.description.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+            foundItems.push(menuItem);
+          }
+        })
+        return foundItems;
+      });
+    }
   }
+
+  function FoundItems() {
+    return {
+      templateUrl: 'foundItems.html',
+      restrict: 'E',
+      scope: {
+        foundItems: '<',
+        onRemove: '&'
+      }
+    }
+  }
+
 })()
