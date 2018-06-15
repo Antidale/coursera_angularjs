37d54437d0aa72c2ed0b74a97f5661c949dd6449
Scott Wyngarden <swyngarden@gmail.com>
2018-06-15 15:39:37 -0700

    adding menudata.service file with a couple of stubbed methods

---
+++ module_4/menudata.service.js
@@ -0,0 +1,17 @@
+(() => {
+  'use strict'
+  angular.module('data').
+    service('MenuDataService', MenuDataService)
+
+  MenuDataService = () => {
+    let service = this;
+
+    service.getAllCategories = () => {
+
+    }
+
+    service.getItemsForCategory = (categoryShortName) => {
+
+    }
+  }
+})
