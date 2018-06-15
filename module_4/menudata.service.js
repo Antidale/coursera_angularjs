4bf2cdd878beaecd15dde0feb19fcb72c3dc01e8
Scott Wyngarden <swyngarden@gmail.com>
2018-06-15 15:48:01 -0700

    added  get requests for getting all categories and getting items for a specific category

--- module_4/menudata.service.js
+++ module_4/menudata.service.js
@@ -8,11 +8,18 @@
     let service = this;
 
     service.getAllCategories = () => {
-
+      return $http({
+        method: 'GET',
+        url: 'https://davids-restaurant.herokuapp.com/categories.json'
+      })
     }
 
     service.getItemsForCategory = (categoryShortName) => {
-
+      let baseURL = 'https://davids-restaurant.herokuapp.com/menu-items.json?category='
+      return $http({
+        method: 'GET',
+        url: baseURL + categoryShortName
+      })
     }
   }
 })
